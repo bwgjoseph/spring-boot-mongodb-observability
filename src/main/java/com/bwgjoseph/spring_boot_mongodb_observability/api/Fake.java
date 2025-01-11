ec109b18050169f25c5c9aaaf4ffc53d589d9e22
bwgjoseph <josephgan@live.com.sg>
2025-01-11 17:05:41 +0800

    add usage of @Observed

--- src/main/java/com/bwgjoseph/spring_boot_mongodb_observability/api/Fake.java
+++ src/main/java/com/bwgjoseph/spring_boot_mongodb_observability/api/Fake.java
@@ -1,5 +1,6 @@
 package com.bwgjoseph.spring_boot_mongodb_observability.api;
 
+import io.micrometer.observation.annotation.Observed;
 import io.micrometer.tracing.annotation.NewSpan;
 import io.micrometer.tracing.annotation.SpanTag;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class Fake {
 
+    @Observed(name = "fake.new.class.span")
     @NewSpan(name = "new class span")
     public void newClassSpan(@SpanTag("arg") String arg) {
       log.info("this will generate a new span");
